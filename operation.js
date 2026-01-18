9e5914710b975383966575fd1232e6cfdd9900b6
iamarifhossain1 <sporshoahmed2667@gmail.com>
2026-01-18 08:54:30 +0600

    task complete

---
+++ operation.js
@@ -0,0 +1,9 @@
+var onionPrice = 20;
+var eggPrice = 10;
+
+var total = onionPrice + eggPrice;
+var total = onionPrice - eggPrice;
+var total = onionPrice * eggPrice;
+var total = onionPrice / eggPrice;
+var total = onionPrice % eggPrice;
+console.log(total);
---
+++ shorthand.js
@@ -0,0 +1,6 @@
+var price = 30;
+// price = price +  20;
+
+price += 10
+
+console.log(price);
