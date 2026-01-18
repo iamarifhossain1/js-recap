a7ab7c8bd6a0a62c73a6c70ac48c076d0d0b6000
iamarifhossain1 <sporshoahmed2667@gmail.com>
2026-01-19 02:54:31 +0600

    file moved

---
+++ Basic Fundamental/number.js
@@ -0,0 +1,12 @@
+var orangePrice = 20;
+var mangoPrice = 0.5;
+// var applePrice = parseInt('22.5');
+var applePrice = parseFloat('22.5');
+
+var totalPrice = orangePrice + applePrice;
+
+console.log(typeof applePrice);
+console.log(typeof orangePrice);
+console.log(totalPrice);
+
+
---
+++ Basic Fundamental/task.js
@@ -0,0 +1,36 @@
+/**
+ * Problem - 1
+ */
+
+const money = 1000;
+const applePrice = 200;
+const orangePrice = 100;
+const totalPrice = applePrice + orangePrice;
+const returnMoney = money - totalPrice
+
+// console.log(returnMoney);
+
+
+/**
+ * Problem - 2
+ */
+
+const math = parseFloat('75.25');
+const biology = 65;
+const chemistry = 80;
+const physics = parseFloat('35.45');
+const bangla = parseFloat('99.50');
+const totalMarks = math + biology + chemistry + physics + bangla;
+const average = totalMarks / 5;
+// console.log(average.toFixed(2));
+
+
+/**
+ * Problem - 3
+ */
+
+const number = 119;
+const divide = 119 % 5;
+
+console.log(divide);
+
