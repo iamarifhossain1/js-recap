5acd3730074bdbd7ea291f2714c1fea447f73655
iamarifhossain1 <sporshoahmed2667@gmail.com>
2026-01-18 08:02:28 +0600

    parseInt-parseFloat-toFixed

--- number.js
+++ number.js
@@ -1,4 +1,12 @@
 var orangePrice = 20;
 var mangoPrice = 0.5;
-var applePrice = 20;
+// var applePrice = parseInt('22.5');
+var applePrice = parseFloat('22.5');
+
+var totalPrice = orangePrice + applePrice;
+
+console.log(typeof applePrice);
+console.log(typeof orangePrice);
+console.log(totalPrice);
+
 
