ee953cb26795a3de8ddeb32e22fd4b41c41b96ee
iamarifhossain1 <sporshoahmed2667@gmail.com>
2026-01-22 02:20:31 +0600

    multiple-conditions

---
+++ JS-Conditionals/multiple-condition.js
@@ -0,0 +1,24 @@
+const salary = 25000;
+const isBCS = true;
+const height = 68;
+const hasCar = true
+
+// if (salary > 20000 || isBCS == true || height > 65) {
+//     console.log('Patro Matched :D');
+    
+// }
+
+// else {
+//     console.log('Patro did not matched :(');
+    
+// }
+
+if ((salary > 24000 && hasCar == true) || isBCS == true) {
+    console.log('Profile Matched');
+    
+}
+
+else {
+    console.log('Profile did not match');
+    
+}
