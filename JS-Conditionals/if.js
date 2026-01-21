5d1025f7f110313e45372a2b936c8d57cf52b23d
iamarifhossain1 <sporshoahmed2667@gmail.com>
2026-01-21 07:48:25 +0600

    if

---
+++ JS-Conditionals/if.js
@@ -0,0 +1,12 @@
+const biriyaniPrice = 300
+const isMutton = true;
+
+if (biriyaniPrice <= 300 && isMutton === false) {
+    console.log('give me a packet of biriyani');
+    
+}
+
+else {
+    console.log('sorry i prefer beef biriyani');
+    
+}
