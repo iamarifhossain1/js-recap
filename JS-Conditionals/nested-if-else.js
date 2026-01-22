8cc46322216ffe727ac893726c76be974c2cc79c
iamarifhossain1 <sporshoahmed2667@gmail.com>
2026-01-22 10:20:07 +0600

    nested-if-else

---
+++ JS-Conditionals/nested-if-else.js
@@ -0,0 +1,20 @@
+const money = 150;
+
+if (money >= 300) {
+    console.log('You are rich :D');
+}
+
+else {
+    if (money >= 200) {
+        console.log('cholo kichu khai :D');
+    }
+
+    else {
+        if (money >= 100) {
+            console.log('Atleast chaa khaoyao? ');
+        }
+        else {
+            console.log('tumi gorib bashay jao -.-');
+        }
+    }
+}
