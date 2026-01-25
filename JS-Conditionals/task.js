abf5124a157d20778753eeba94f446e0816e0a0e
iamarifhossain1 <sporshoahmed2667@gmail.com>
2026-01-26 02:46:25 +0600

    task

--- JS-Conditionals/task.js
+++ JS-Conditionals/task.js
@@ -54,39 +54,91 @@
 // }
 
 
-const myScore = 95;
-const friendScore = 35
+// const myScore = 95;
+// const friendScore = 35
 
-if (myScore > 80) {
-    if (friendScore > 80) {
-        console.log('Dost chol ek sathe lunch kori :D');
-    }
+// if (myScore > 80) {
+//     if (friendScore > 80) {
+//         console.log('Dost chol ek sathe lunch kori :D');
+//     }
 
-    else {
-        if (friendScore < 80 && friendScore >= 60) {
-            console.log('its okay dost :( better luck next time');
+//     else {
+//         if (friendScore < 80 && friendScore >= 60) {
+//             console.log('its okay dost :( better luck next time');
+//         }
+
+//         else {
+//             if (friendScore < 60 && friendScore >= 40) {
+//                 console.log('dont seen his message -.-');
+//             }
+
+//             else {
+//                 if (friendScore < 40) {
+//                     console.log('Block him/her');
+//                 }
+//             }
+//         }
+//     }
+    
+// }
+
+// else { 
+//     if (myScore < 80) {
+//         console.log('Go Home...sleep and act sad :(');
+        
+//     }
+// }
+
+
+// const num1 = 20;
+// const num2 = 40
+// const result = num1 + num2;
+
+// if (num1 > num2) {
+//     multiply = num1 * 2;
+//     console.log(multiply);
+    
+// }
+
+
+// else {
+//     console.log(result);
+    
+// }
+
+// num1 > num2 ? num1 * 2 : console.log(result);
+
+
+let ticketFare = 800;
+const age = 60;
+const seniorCitizen = 60
+const isStudent = false;
+
+if (age < 10) {
+     console.log('Ticket Free');
+     
+}
+
+else {
+        if( isStudent) {
+            // 50% discount
+            const discount = ticketFare * 50 / 100;
+            const payAmount = ticketFare - discount;
+            console.log(payAmount);
+            
         }
 
         else {
-            if (friendScore < 60 && friendScore >= 40) {
-                console.log('dont seen his message -.-');
+            if( seniorCitizen >= 60) {
+                // 15% discount
+                const discount = ticketFare * 15 / 100;
+                const payAmount = ticketFare - discount;
+                console.log(payAmount);
             }
 
             else {
-                if (friendScore < 40) {
-                    console.log('Block him/her');
-                }
+                console.log(ticketFare);
+                
             }
         }
     }
-    
-}
-
-else { 
-    if (myScore < 80) {
-        console.log('Go Home...sleep and act sad :(');
-        
-    }
-}
-
-console.log(myScore);
