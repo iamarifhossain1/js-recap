c0d94e17969949cc78808d36baf65fee42d65fc5
iamarifhossain1 <sporshoahmed2667@gmail.com>
2026-01-27 09:36:53 +0600

    loop

---
+++ Loop/for-of-loop.js
@@ -0,0 +1,13 @@
+const numbers = [2,4,6,8,10,12];
+
+for (const number of numbers) {
+    console.log(number);
+    
+}
+
+const fruits = ['orange', 'mango', 'pineapple', 'watermelon', 'coconut'];
+
+for (const fruit of fruits) {
+    console.log(fruit);
+    
+}
