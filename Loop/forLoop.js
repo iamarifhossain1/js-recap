270a4a52e4bfa7e3c85cec7852c60135d22ad6c5
iamarifhossain1 <sporshoahmed2667@gmail.com>
2026-01-30 04:10:31 +0600

    for loop

---
+++ Loop/forLoop.js
@@ -0,0 +1,19 @@
+for (let num = 0; num <= 5; num++) {
+    console.log(num);
+    
+}
+
+
+// Even Number
+
+for (let i = 0; i <= 20; i = i + 2) {
+    console.log(i);
+    
+}
+
+// Odd NUmber
+
+for (let i = 1; i <= 20; i = i + 2) {
+    console.log(i);
+    
+}
