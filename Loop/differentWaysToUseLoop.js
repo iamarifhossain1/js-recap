f46d2abebcbf140d8e7ba3380f94befbf8bed092
iamarifhossain1 <sporshoahmed2667@gmail.com>
2026-02-04 23:27:13 +0600

    divisible

--- Loop/differentWaysToUseLoop.js
+++ Loop/differentWaysToUseLoop.js
@@ -1,41 +1,41 @@
-// for (let i = 0; i <= 50; i = i + 1) {
-//     if (i % 2 === 0) {
-//         console.log(i);
+for (let i = 0; i <= 50; i = i + 1) {
+    if (i % 2 === 0) {
+        console.log(i);
         
-//     }
-// }
+    }
+}
 
-// for (let i = 0; i <= 50; i = i + 1) {
-//     if (i % 2 !== 0) {
-//         console.log(i);
+for (let i = 0; i <= 50; i = i + 1) {
+    if (i % 2 !== 0) {
+        console.log(i);
         
-//     }
-// }
+    }
+}
 
 
-// for (let i = 0; i <= 40; i = i + 1) {
-//     if (i % 5 === 0) {
-//         console.log(i);
+for (let i = 0; i <= 40; i = i + 1) {
+    if (i % 5 === 0) {
+        console.log(i);
         
-//     }
-// }
+    }
+}
 
-// for (let i = 0; i <= 200; i = i + 1) {
-//     if (i % 5 === 0 || i % 3 === 0) {
-//         console.log(i);
+for (let i = 0; i <= 200; i = i + 1) {
+    if (i % 5 === 0 || i % 3 === 0) {
+        console.log(i);
         
-//     }
-// }
+    }
+}
 
-// let sum = 0;
+let sum = 0;
 
-// for (let i = 0; i <= 20; i = i + 1) {
-//     if (i % 5 === 0) {
-//         sum = sum + i;
-//         console.log('total sum of number:', sum);
+for (let i = 0; i <= 20; i = i + 1) {
+    if (i % 5 === 0) {
+        sum = sum + i;
+        console.log('total sum of number:', sum);
         
-//     }
-// }
+    }
+}
 
 
 let addition = 0
