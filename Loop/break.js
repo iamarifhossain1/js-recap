193c9322a5c29d3c9ff010a8a260f55ef0f3c08d
iamarifhossain1 <sporshoahmed2667@gmail.com>
2026-02-05 11:36:59 +0600

    break-continue

---
+++ Loop/break.js
@@ -0,0 +1,19 @@
+for (let i = 0; i <= 20; i++) {
+    if (i >= 15) {
+        break
+    }
+    console.log(i);
+    
+}
+
+let n = 50; 
+
+while (n >= 30) {
+    if (n <= 40) {
+        break;
+    }
+
+    n--;
+    console.log(n);
+    
+}
---
+++ Loop/continue.js
@@ -0,0 +1,21 @@
+for (let i = 0; i <= 20; i++) {
+    if (i % 2 === 0) {
+        continue;
+    }
+
+    console.log(i);
+    
+}
+
+
+let  n = 0;
+
+while (n <= 20) {
+    n++;
+    if (n % 2 === 1) {
+        continue
+    }
+
+    console.log(n);
+    
+}
