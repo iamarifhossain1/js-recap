a706fd28e9e9f10a6b431ff4e679071bcb5b5295
iamarifhossain1 <sporshoahmed2667@gmail.com>
2026-02-10 02:06:04 +0600

    for-task

--- Loop/for-task.js
+++ Loop/for-task.js
@@ -4,10 +4,10 @@
 
  */
 
-// for (let i = 1; i <= 60; i++) {
-//     console.log('I will invest at least 6 hrs every single day for next 60 days!', i);
+for (let i = 1; i <= 60; i++) {
+    console.log('I will invest at least 6 hrs every single day for next 60 days!', i);
     
-// }
+}
 
 
 /***
@@ -18,19 +18,19 @@ Find all the odd numbers from 61 to 100.
 
  */
 
-// for (let i = 61; i <= 100; i+=2) {
-//     console.log(i);
+for (let i = 61; i <= 100; i+=2) {
+    console.log(i);
     
-// }
+}
 
 // another way using modulus 
 
-// for (let i = 61; i <= 100; i++) {
-//     if (i % 2 === 1) {
-//         console.log(i);
+for (let i = 61; i <= 100; i++) {
+    if (i % 2 === 1) {
+        console.log(i);
         
-//     }
-// }
+    }
+}
 
 
 /***
@@ -41,20 +41,20 @@ Find all the even numbers from 78 to 98.
 
  */
 
-// for (let i = 78; i <= 98; i+=2) {
-//     console.log(i);
+for (let i = 78; i <= 98; i+=2) {
+    console.log(i);
     
     
-// }
+}
 
 // another way using modulus
 
-// for (let i = 78; i <= 98; i++) {
-//     if (i % 2 === 0) {
-//         console.log(i);
+for (let i = 78; i <= 98; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
         
-//     }
-// }
+    }
+}
 
 
 
@@ -66,16 +66,16 @@ Display sum of all the odd numbers from 91 to 129.
 
  */
 
-// let sum = 0;
+let sum = 0;
 
-// for (let i = 91; i <= 129; i++) {
-//     if (i % 2 === 1) {
-//         console.log(i);
-//         sum = sum + i;
-//     }
-// }
+for (let i = 91; i <= 129; i++) {
+    if (i % 2 === 1) {
+        console.log(i);
+        sum = sum + i;
+    }
+}
 
-// console.log('total sum of 91 to 129:', sum);
+console.log('total sum of 91 to 129:', sum);
 
 
 /***
@@ -86,14 +86,14 @@ Display sum of all the even numbers from 51 to 85.
 
  */
 
-// for (i = 51; i <= 85; i++) {
-//     if (i % 2 === 0) {
-//         console.log(i);
-//         sum = sum + i;   
-//     }
-// }
+for (i = 51; i <= 85; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+        sum = sum + i;   
+    }
+}
 
-// console.log('total sum of 51 to 85:', sum);
+console.log('total sum of 51 to 85:', sum);
 
 
 /***
@@ -102,12 +102,12 @@ Generate a multiplication table for number 9
 
  */
 
-// let multiplication = 9;
+let multiplication = 9;
 
-// for (let i = 1; i <= 10; i++) {
-//     console.log(multiplication + ' x ' + i + ' = ' + (multiplication * i));
+for (let i = 1; i <= 10; i++) {
+    console.log(multiplication + ' x ' + i + ' = ' + (multiplication * i));
     
-// }
+}
 
 
 /***
