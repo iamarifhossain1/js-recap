cb5128bc306584f202d1d9773920a251bc2585db
iamarifhossain1 <sporshoahmed2667@gmail.com>
2026-02-09 16:15:10 +0600

    break-task

--- Loop/break-task.js
+++ Loop/break-task.js
@@ -4,32 +4,77 @@ Write a loop 1 to 200. Use break to exit the loop once you find 100.
 
 */
 
-// for (let i = 1; i <= 200; i++) {
-//     if (i > 100) {
-//         break;
-//     }
+// Plain For Loop
 
-//     console.log(i);
-    
-// }
-
-/* 
-
-Write a while loop that adds numbers starting from 1, but stops (using break) as soon as the sum reaches or exceeds 100
-
-*/
-
-let i = 1;
-let sum = 0;
-
-while (i <= 100) {
-    sum = sum + 1;
-    if (sum >= 100) {
+for (let i = 1; i <= 200; i++) {
+    if (i >= 100) {
         break;
     }
-    i++
+
+    console.log(i);
+    
 }
 
-console.log('total sum:', sum);
-console.log('total sum:', sum);
-console.log('total sum:', sum);
+
+// While Loop
+
+let i = 0;
+
+while (i <= 200) {
+    if(i >= 100) {
+        break;
+    }
+
+    i++;
+
+    console.log(i);
+    
+}
+
+
+
+/* 
+Write a while loop that adds numbers starting from 1, but stops (using break) as soon as the sum reaches or exceeds 100
+*/
+
+let num = 1;
+let sum = 0;
+
+while (num <= 100) {
+    sum = sum + num;
+     console.log('total sum:', sum);
+    if (sum >= 100) {
+        break;
+        
+    }
+
+    num++;
+    
+}
+
+
+let sum1 = 0;
+
+for (let i = 1; i <= 100; i++) {
+    if (sum1 >= 100) {
+        
+        break;
+    }   
+    sum1 = sum1 + i;
+
+    console.log('total sum:', sum1);
+    
+}
+
+
+let sqrNum = 1;
+
+while (sqrNum <= 100) {
+    if (Math.sqrt(sqrNum) % 1 === 0 && sqrNum !== 1) {
+        break;
+    }
+
+    console.log(sqrNum);
+    sqrNum++
+    
+}
