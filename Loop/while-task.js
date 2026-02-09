191379d5d03bf628e85d7e6d720a3d0e1459e260
iamarifhossain1 <sporshoahmed2667@gmail.com>
2026-02-10 02:24:06 +0600

    while-loop-task

--- Loop/while-task.js
+++ Loop/while-task.js
@@ -4,10 +4,48 @@
 
  */
 
-let i = 1;
+// let i = 1;
 
-while (i <= 60) {
-    console.log('I will invest at least 6 hrs every single day for next 60 days!', i);   
-    i++;
-}
+// while (i <= 60) {
+//     console.log('I will invest at least 6 hrs every single day for next 60 days!', i);   
+//     i++;
+// }
+
+
+/***
+
+Subtask-1:
+
+Find all the odd numbers from 61 to 100.
+
+ */
+
+// let oddNum = 61;
+
+// while (oddNum <= 100) {
+//     if (oddNum % 2 === 1) {
+//         console.log('Odd numbers are from 61 to 100:', oddNum);
+//     }
+
+//     oddNum++;
+// }
+
+
+/***
+
+Subtask-2:
+
+Find all the even numbers from 78 to 98.
+
+ */
+
+// let evenNum = 78;
+
+// while (evenNum <= 98) {
+//     if (evenNum % 2 === 0) {
+//         console.log('Even numbers are from 78 to 98:', evenNum);
+//     }
+
+//     evenNum++;
+// }
 
