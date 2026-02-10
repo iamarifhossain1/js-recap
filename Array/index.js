0f66d6aef9408415584cae28f7c77f302ca4b303
iamarifhossain1 <sporshoahmed2667@gmail.com>
2026-02-10 20:21:08 +0600

    array-length-index

---
+++ Array/index.js
@@ -0,0 +1,10 @@
+const numbers = [20, 30, 40, 50, 60, 70]
+
+// console.log(numbers);
+console.log(numbers[3]);
+
+const fourth = numbers[3];
+console.log(fourth);
+
+numbers[3] = 500;
+console.log(numbers);
---
+++ Array/length.js
@@ -0,0 +1,2 @@
+const numbers = [2,4,5,6,7,8,10,12,19];
+console.log(numbers.length);
