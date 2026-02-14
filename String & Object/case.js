83bde148149b774b86457a00ed2e55d50b0ca5e7
iamarifhossain1 <sporshoahmed2667@gmail.com>
2026-02-14 10:53:37 +0600

    case

---
+++ String & Object/case.js
@@ -0,0 +1,23 @@
+const book = 'chemistry'
+const subject = 'ChemisTry'
+
+if (book.toLocaleLowerCase() === subject.toLowerCase()) {
+    console.log('Book found');
+}
+
+else {
+    console.log('book not found');
+    
+}
+
+const drinks = 'water';
+const liquid = '   water'
+
+if (drinks.trim() === liquid.trim()) {
+    console.log('Paniiiiiiiiii');
+}
+
+else {
+    console.log('Cokeeeeeeeee');
+    
+}
