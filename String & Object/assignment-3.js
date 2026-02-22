c5c0bcd367bc1da00db0328cf530e8e6c85017dd
iamarifhossain1 <sporshoahmed2667@gmail.com>
2026-02-22 10:01:02 +0600

    assignment-3

--- String & Object/assignment-3.js
+++ String & Object/assignment-3.js
@@ -1,3 +1,73 @@
-let area = 800;
+/**
+ * Task 1: Divide the Asset
+ */
+
+const area = 800;
 const divide = area / 2;
-console.log(divide);
+// console.log(divide);
+
+/**
+ * Task 2: Cycle or  Laptop
+ */
+
+
+const money = 31000;
+
+if (money >= 25000) {
+    // console.log('Laptop');
+}
+
+else {
+    if (money >= 10000) {
+        // console.log('Cycle');
+    }
+
+    else {
+        // console.log('Chocolate');        
+    }
+}
+
+
+/**
+ * Task 3: Medicine Planner
+ */
+
+const lastDay = 11;
+
+for (let i = 1; i <= lastDay; i++) {
+    if (i % 3 === 0) {
+        // console.log(i, '-', 'Medicine');
+    }
+
+    else {
+        // console.log(i, '-', 'Rest');
+        
+    }
+} 
+
+/**
+ * Task 4: Delete / Store
+ */
+
+const fileName = "pdfData.jpeg";
+
+if (fileName.startsWith('#')) {
+    console.log('Store');
+}
+
+else {
+    if (fileName.endsWith('pdf')) {
+        console.log('Store');
+    }
+
+    else {
+        if (fileName.endsWith('docx')) {
+            console.log('Store');
+        }
+
+        else {
+            console.log('Delete');
+            
+        }
+    }
+}
