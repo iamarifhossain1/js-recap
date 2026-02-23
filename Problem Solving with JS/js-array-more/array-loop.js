ce4965c1b05492181fb09984b84e2ca371a93ed3
iamarifhossain1 <sporshoahmed2667@gmail.com>
2026-02-24 04:43:05 +0600

    array-traverse

---
+++ Problem Solving with JS/js-array-more/array-loop.js
@@ -0,0 +1,44 @@
+const fruits = ['apple', 'banana', 'Watermelon', 'Coconut', 'Pineapple'];
+
+
+for (let i = 0; i < fruits.length; i++) {
+    // console.log(i);
+    // console.log(fruits[i]);
+        
+}
+
+
+const numbers = [12, 15, 78, 48, 20, 35];
+
+for (let i = 0; i < numbers.length; i++) {
+    // console.log(i);
+    // console.log(numbers[i]);
+}
+
+let k = 0;
+
+while (k < fruits.length) {
+    // console.log(fruits[k]);
+    k++
+}
+
+let a = 0;
+
+while (a < numbers.length) {
+    // console.log(numbers[a]);
+    a++
+}
+
+const prices = [20, 15, 21, 96, 57, 26, 9];
+
+let b = 0
+
+while (b < prices.length) {
+    // console.log(prices[b]);
+    b++;
+}
+
+for (let i = 0; i < prices.length; i++) {
+    console.log(prices);
+    
+}
