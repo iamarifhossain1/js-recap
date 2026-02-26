5a74e32160c42ee225e720e8921107316141450b
iamarifhossain1 <sporshoahmed2667@gmail.com>
2026-02-26 17:49:57 +0600

    array-reverse

--- Problem Solving with JS/js-array-more/array-reverse.js
+++ Problem Solving with JS/js-array-more/array-reverse.js
@@ -1,8 +1,34 @@
-const names=  'arif hossain';
-let reverse = '';
+const numbers = [1, 2, 3, 4, 5];
+const reverse = [];
 
-for (const name of names) {
-    reverse = name + reverse;
+for (let i = 0; i < numbers.length; i ++) {
+    console.log(numbers[i]);
+    reverse.unshift(numbers[i])
 }
 
 console.log(reverse);
+
+
+const names = ['Arif', 'Hanif', 'Mousumi', 'Ayra', 'Hafsa', 'Muyaz'];
+
+for (let i = 0; i < names.length; i++) {
+    reverse.unshift(names[i]);
+}
+
+console.log(reverse);
+
+
+const fruits = ['Apple', 'Banana', 'Pineapple', 'Watermelon', 'Strawberry']
+
+for (const fruit of fruits) {
+    reverse.unshift(fruit);
+}
+
+console.log(reverse);
+
+
+const uniqueNumbers = [20, 40, 60, 80, 100];
+
+uniqueNumbers.reverse();
+console.log(uniqueNumbers);
+
